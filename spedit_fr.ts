812d6185531b5b2f79c3737eb6608e57d6c9b45c
Ben Forsyth <benf@filewave.com>
2012-12-11 11:43:59 -0800

    initial commit of translation files

---
+++ spedit_fr.ts
@@ -0,0 +1,237 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>PrefsEditorDlg</name>
+    <message>
+        <location filename="../prefseditordlg.ui" line="14"/>
+        <source>Superprefs Editor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="20"/>
+        <source>These preferences will be merged with the preferences on target clients.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="27"/>
+        <source>If you would like a value to remain unchanged, leave it blank.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="38"/>
+        <source>Communications</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="44"/>
+        <source>FileWave Server Address:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="54"/>
+        <source>Server Port:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="64"/>
+        <source>Monitor Port:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="74"/>
+        <source>Tickle Interval:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="84"/>
+        <source>Request Interval:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="94"/>
+        <source>Synchronize Client Name with Computer name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="127"/>
+        <source>ex. 20010</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="134"/>
+        <source>ex. 20015</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="141"/>
+        <location filename="../prefseditordlg.ui" line="151"/>
+        <location filename="../prefseditordlg.ui" line="553"/>
+        <location filename="../prefseditordlg.ui" line="576"/>
+        <location filename="../prefseditordlg.ui" line="610"/>
+        <location filename="../prefseditordlg.ui" line="652"/>
+        <source>_</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="167"/>
+        <location filename="../prefseditordlg.ui" line="174"/>
+        <source>seconds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="226"/>
+        <source>Kiosk Port:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="239"/>
+        <source>ex. 20020</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="247"/>
+        <source>Boosters</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="292"/>
+        <source>IP or DNS Address:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="299"/>
+        <source>Port</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="306"/>
+        <source>Booster 1:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="338"/>
+        <source>Booster 2:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="348"/>
+        <source>Booster 3:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="358"/>
+        <source>Booster 4:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="368"/>
+        <source>Booster 5:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="487"/>
+        <source>Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="493"/>
+        <source>Debug Level:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="503"/>
+        <source>File Check Interval:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="513"/>
+        <source>Free Space Margin:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="523"/>
+        <source>Password:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="533"/>
+        <source>Verify Password:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="543"/>
+        <source>Priority:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="569"/>
+        <source>minutes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.ui" line="589"/>
+        <source>MB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.cpp" line="108"/>
+        <source>Select an existing fwcld.newprefs.plist file or click Cancel to start with a blank one...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.cpp" line="170"/>
+        <source>Password verification failed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.cpp" line="176"/>
+        <source>Select a location to save the Superprefs file;  You&apos;ll add it to a fileset later.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prefseditordlg.cpp" line="298"/>
+        <location filename="../prefseditordlg.cpp" line="325"/>
+        <source>You have unsaved changes.  Are you sure you want to quit?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="15"/>
+        <source>FileWave</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="16"/>
+        <source>filewave.com</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="17"/>
+        <source>SuperPrefs Editor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="25"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="26"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="27"/>
+        <source>Ctrl+W</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="28"/>
+        <source>About %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
