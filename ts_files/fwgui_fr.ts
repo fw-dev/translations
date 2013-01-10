cdee215a630ef905c41dc3c736a16ecfe52788d4
Thierry Bastian <thierryb@filewave.com>
2013-01-10 16:12:14 +0100

    FW-0000 re update French Translation

--- ts_files/fwgui_fr.ts
+++ ts_files/fwgui_fr.ts
@@ -7,17 +7,17 @@
         <location filename="../../QtGUIInstaller/Installer.cpp" line="49"/>
         <location filename="../../QtGUIInstaller/Installer.cpp" line="59"/>
         <source>FileWave Kiosk</source>
-        <translation type="unfinished"></translation>
+        <translation>Kiosque FileWave</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/Installer.cpp" line="79"/>
         <source>Available Updates: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Mises à jour disponibles: %1</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/Installer.cpp" line="83"/>
         <source>Install Software...</source>
-        <translation type="unfinished"></translation>
+        <translation>Installation du logiciel...</translation>
     </message>
 </context>
 <context>
@@ -25,27 +25,27 @@
     <message>
         <location filename="../../QtGUIInstaller/InstallerStatusDialog.ui" line="26"/>
         <source>FileWave Installer</source>
-        <translation type="unfinished"></translation>
+        <translation>Installeur FileWave</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/InstallerStatusDialog.ui" line="41"/>
         <source>Important system software is now being installed on your computer...</source>
-        <translation type="unfinished"></translation>
+        <translation>Un logiciel système est en cours d&apos;installation sur votre ordinateur...</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/InstallerStatusDialog.ui" line="51"/>
         <source>Do not shut down your computer.  It will automatically reboot when the installation is complete.</source>
-        <translation type="unfinished"></translation>
+        <translation>N&apos;éteigne pas votre ordinateur. Il redémarrera automatiquement après l&apos;installation.</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/InstallerStatusDialog.ui" line="64"/>
         <source>Do not use the start menu.</source>
-        <translation type="unfinished"></translation>
+        <translation>N&apos;utilisez pas le menu de démarrage.</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/InstallerStatusDialog.ui" line="74"/>
         <source>Activating Fileset(s)...</source>
-        <translation type="unfinished"></translation>
+        <translation>Activation du/des fileset(s)...</translation>
     </message>
 </context>
 <context>
@@ -53,47 +53,47 @@
     <message>
         <location filename="../../QtGUIInstaller/Kiosk.ui" line="14"/>
         <source>FileWave Kiosk</source>
-        <translation type="unfinished"></translation>
+        <translation>Kiosque FileWave</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/Kiosk.cpp" line="73"/>
         <source>Install</source>
-        <translation type="unfinished"></translation>
+        <translation>Installer</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/Kiosk.cpp" line="75"/>
         <source>Uninstall</source>
-        <translation type="unfinished"></translation>
+        <translation>Désinstaller</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/Kiosk.cpp" line="77"/>
         <source>Reinstall</source>
-        <translation type="unfinished"></translation>
+        <translation>Réinstaller</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/Kiosk.cpp" line="193"/>
         <source>Search...</source>
-        <translation type="unfinished"></translation>
+        <translation>Recherche...</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/Kiosk.cpp" line="197"/>
         <source>All Items</source>
-        <translation type="unfinished"></translation>
+        <translation>Tous les éléments</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/Kiosk.cpp" line="198"/>
         <source>Installed</source>
-        <translation type="unfinished"></translation>
+        <translation>Installé</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/Kiosk.cpp" line="199"/>
         <source>Not Installed</source>
-        <translation type="unfinished"></translation>
+        <translation>Non installé</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/Kiosk.cpp" line="603"/>
         <source>%1 Item%2</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 élément%2</translation>
     </message>
 </context>
 <context>
@@ -101,83 +101,83 @@
     <message>
         <location filename="../../QtGUIInstaller/KioskInstallWidget.ui" line="20"/>
         <source>FileWave Kiosk</source>
-        <translation type="unfinished"></translation>
+        <translation>Kiosque FileWave</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/KioskInstallWidget.ui" line="56"/>
         <source>icon</source>
-        <translation type="unfinished"></translation>
+        <translation>icone</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/KioskInstallWidget.ui" line="102"/>
         <source>Title (Fileset Name)</source>
-        <translation type="unfinished"></translation>
+        <translation>Titre (nom du fileset)</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/KioskInstallWidget.ui" line="121"/>
         <source>Fileset Category</source>
-        <translation type="unfinished"></translation>
+        <translation>Catégorie du fileset</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/KioskInstallWidget.ui" line="232"/>
         <source>    Queued for Installation    </source>
-        <translation type="unfinished"></translation>
+        <translation>Mis dans la file d&apos;installation</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/KioskInstallWidget.ui" line="305"/>
         <location filename="../../QtGUIInstaller/KioskInstallWidget.cpp" line="96"/>
         <source>UNINSTALL</source>
-        <translation type="unfinished"></translation>
+        <translation>DESINSTALLER</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/KioskInstallWidget.cpp" line="92"/>
         <source>INSTALL</source>
-        <translation type="unfinished"></translation>
+        <translation>INSTALLER</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/KioskInstallWidget.cpp" line="125"/>
         <source>Not Installed</source>
-        <translation type="unfinished"></translation>
+        <translation>Non installé</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/KioskInstallWidget.cpp" line="127"/>
         <source>Queued for Installation</source>
-        <translation type="unfinished"></translation>
+        <translation>Mis dans la file d&apos;installation</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/KioskInstallWidget.cpp" line="129"/>
         <source>Downloading...</source>
-        <translation type="unfinished"></translation>
+        <translation>Téléchargement en cours...</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/KioskInstallWidget.cpp" line="131"/>
         <source>Installing...</source>
-        <translation type="unfinished"></translation>
+        <translation>Installation en cours...</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/KioskInstallWidget.cpp" line="133"/>
         <source>Installed</source>
-        <translation type="unfinished"></translation>
+        <translation>Installé</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/KioskInstallWidget.cpp" line="135"/>
         <source>Uninstalling...</source>
-        <translation type="unfinished"></translation>
+        <translation>Desinstallation en cours...</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/KioskInstallWidget.cpp" line="137"/>
         <source>Reinstalling...</source>
-        <translation type="unfinished"></translation>
+        <translation>Réinstallation en cours...</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/KioskInstallWidget.cpp" line="139"/>
         <source>Install Failed</source>
-        <translation type="unfinished"></translation>
+        <translation>L&apos;installation a échoué</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/KioskInstallWidget.cpp" line="141"/>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>Inconnu</translation>
     </message>
 </context>
 <context>
@@ -185,77 +185,77 @@
     <message>
         <location filename="../../QtGUIInstaller/QuitRunningProcessDialog.ui" line="20"/>
         <source>Quit All Running Processes</source>
-        <translation type="unfinished"></translation>
+        <translation>Terminer tous les processus en cours d&apos;exécution</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/QuitRunningProcessDialog.ui" line="32"/>
         <source>Important System Software is to be installed on this computer...</source>
-        <translation type="unfinished"></translation>
+        <translation>Une mise à jour importante du système doit être installée sur cet ordinateur...</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/QuitRunningProcessDialog.ui" line="39"/>
         <source>Please save your work and exit all programs.</source>
-        <translation type="unfinished"></translation>
+        <translation>Veuillez sauvegarder tout travail en cours et quitter tous les programmes.</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/QuitRunningProcessDialog.ui" line="46"/>
         <source>The following applications are preventing this installation from taking place.</source>
-        <translation type="unfinished"></translation>
+        <translation>Les applications suivantes empêchent l&apos;installation de s&apos;exécuter.</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/QuitRunningProcessDialog.ui" line="101"/>
         <source>Quit Application</source>
-        <translation type="unfinished"></translation>
+        <translation>Quitter l&apos;application</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/QuitRunningProcessDialog.ui" line="114"/>
         <source>Quit All</source>
-        <translation type="unfinished"></translation>
+        <translation>Quitter toutes les applications</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/QuitRunningProcessDialog.ui" line="137"/>
         <source>Delay</source>
-        <translation type="unfinished"></translation>
+        <translation>Retarder</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/QuitRunningProcessDialog.ui" line="150"/>
         <source>for:</source>
-        <translation type="unfinished"></translation>
+        <translation>pour :</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/QuitRunningProcessDialog.ui" line="186"/>
         <source>Start Installation</source>
-        <translation type="unfinished"></translation>
+        <translation>Démarrer l&apos;installation</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/QuitRunningProcessDialog.cpp" line="62"/>
         <source>15 Minutes</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/QuitRunningProcessDialog.cpp" line="63"/>
         <source>30 Minutes</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/QuitRunningProcessDialog.cpp" line="64"/>
         <source>1 Hour</source>
-        <translation type="unfinished"></translation>
+        <translation>1 Heure</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/QuitRunningProcessDialog.cpp" line="65"/>
         <source>2 Hours</source>
-        <translation type="unfinished"></translation>
+        <translation>2 Heures</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/QuitRunningProcessDialog.cpp" line="102"/>
         <source>%1 programs active</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 programme(s) actif(s)</translation>
     </message>
     <message>
         <location filename="../../QtGUIInstaller/QuitRunningProcessDialog.cpp" line="118"/>
         <source>Installation will begin automatically in %1 seconds...</source>
-        <translation type="unfinished"></translation>
+        <translation>L&apos;installation débutera automatiquement dans %1 secondes...</translation>
     </message>
 </context>
 <context>
@@ -263,7 +263,7 @@
     <message>
         <location filename="../../QtGUIInstaller/TopTenScrollArea.cpp" line="29"/>
         <source>Top Downloads</source>
-        <translation type="unfinished"></translation>
+        <translation>Top des téléchargements</translation>
     </message>
 </context>
 </TS>
