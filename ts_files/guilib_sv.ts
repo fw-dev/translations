46daecd91b3e74607c44b587446571ebefeda82d
Thierry Bastian <thierryb@filewave.com>
2015-10-13 00:33:18 +0200

    FW-0000 adding swedish translation file

---
+++ ts_files/guilib_sv.ts
@@ -0,0 +1,455 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="sv_SE">
+<context>
+    <name>Boosters</name>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Boosters.ui" line="13"/>
+        <source>Form</source>
+        <translatorcomment>???</translatorcomment>
+        <translation>Form</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Boosters.ui" line="36"/>
+        <source>IP or DNS Address</source>
+        <translation>IP eller DNS-adress</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Boosters.ui" line="46"/>
+        <source>Port</source>
+        <translation>Port</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Boosters.ui" line="56"/>
+        <source>Booster 1:</source>
+        <translation>Booster 1:</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Boosters.ui" line="79"/>
+        <source>Booster 2:</source>
+        <translation>Booster 2:</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Boosters.ui" line="102"/>
+        <source>Booster 3:</source>
+        <translation>Booster 3:</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Boosters.ui" line="125"/>
+        <source>Booster 4:</source>
+        <translation>Booster 4:</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Boosters.ui" line="148"/>
+        <source>Booster 5:</source>
+        <translation>Booster 5:</translation>
+    </message>
+</context>
+<context>
+    <name>CCommunications</name>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Communications.ui" line="14"/>
+        <source>Communications</source>
+        <translation>Kommunikation</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Communications.ui" line="51"/>
+        <source>Monitor Port:</source>
+        <translation>Monitor-port:</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Communications.ui" line="137"/>
+        <source>seconds</source>
+        <translation>sekunder</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Communications.ui" line="144"/>
+        <location filename="../ClientStatusMonitorFiles/Communications.ui" line="258"/>
+        <source>Server Port:</source>
+        <translation>Server-port:</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Communications.ui" line="74"/>
+        <source>Tickle Interval:</source>
+        <translation>Anslutningsintervall:</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Communications.ui" line="84"/>
+        <source>ex. 20015</source>
+        <translation>ex. 20015</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Communications.ui" line="101"/>
+        <source>Kiosk Port:</source>
+        <translation>Kiosk-port:</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Communications.ui" line="127"/>
+        <source>FileWave Server Address:</source>
+        <translation>Server-address:</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Communications.ui" line="170"/>
+        <source>ex. 20010</source>
+        <translation>ex. 20010</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Communications.ui" line="177"/>
+        <source>Client Name:</source>
+        <translation>Klient-namn:</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Communications.ui" line="187"/>
+        <source>ex. 20020</source>
+        <translation>ex. 20020</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Communications.ui" line="209"/>
+        <source>Sync computer name</source>
+        <translation>Synkronisera datornamn</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Communications.ui" line="216"/>
+        <source>Use SSL</source>
+        <translation>Använd SSL</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Communications.ui" line="238"/>
+        <source>Inventory Server Address:</source>
+        <translatorcomment>???</translatorcomment>
+        <translation>Inventory-adress:</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Communications.ui" line="251"/>
+        <source>Sync with FileWave</source>
+        <translatorcomment>???</translatorcomment>
+        <translation>Synkronisera med FileWave</translation>
+    </message>
+</context>
+<context>
+    <name>CFWStatusDlg</name>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/FWStatusDlg.ui" line="13"/>
+        <source>Client Monitor</source>
+        <translation>Klientstatus</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/FWStatusDlg.ui" line="33"/>
+        <source>Name:</source>
+        <translation>Namn:</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/FWStatusDlg.ui" line="75"/>
+        <source>Address:</source>
+        <translation>Adress:</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/FWStatusDlg.ui" line="85"/>
+        <location filename="../ClientStatusMonitorFiles/FWStatusDlg.cpp" line="85"/>
+        <source>127.0.0.1</source>
+        <translation>127.0.0.1</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/FWStatusDlg.ui" line="104"/>
+        <source>Port:</source>
+        <translation>Port:</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/FWStatusDlg.ui" line="142"/>
+        <source>Version:</source>
+        <translation>Version:</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/FWStatusDlg.ui" line="180"/>
+        <source>Platform:</source>
+        <translation>OS-version:</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/FWStatusDlg.ui" line="273"/>
+        <source>FileWave Server:</source>
+        <translation>Server:</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/FWStatusDlg.ui" line="289"/>
+        <source>Model Version:</source>
+        <translation>Modell nr:</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/FWStatusDlg.ui" line="305"/>
+        <source>Status:</source>
+        <translation>Status:</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/FWStatusDlg.ui" line="366"/>
+        <source>File:</source>
+        <translation>Fil:</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/FWStatusDlg.ui" line="419"/>
+        <source>Verify</source>
+        <translation>Verifiera</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/FWStatusDlg.ui" line="442"/>
+        <source>Client Log</source>
+        <translation>Klient-logfil</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/FWStatusDlg.ui" line="465"/>
+        <source>Preferences...</source>
+        <translation>Inställningar…</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/FWStatusDlg.cpp" line="164"/>
+        <location filename="../ClientStatusMonitorFiles/FWStatusDlg.cpp" line="485"/>
+        <source>[disconnected]</source>
+        <translation>[ej ansluten - eller fel adress]</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/FWStatusDlg.cpp" line="174"/>
+        <source>Getting Client Prefs...</source>
+        <translation>Hämtar klientinställningar…</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/FWStatusDlg.cpp" line="319"/>
+        <source>%1 - Client Monitor</source>
+        <translation>%1 - Klientstatus</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/FWStatusDlg.cpp" line="464"/>
+        <location filename="../ClientStatusMonitorFiles/FWStatusDlg.cpp" line="467"/>
+        <source> (%1.%2)</source>
+        <translation> (%1.%2)</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/FWStatusDlg.cpp" line="470"/>
+        <source> (%1.%2.%3)</source>
+        <translation> (%1.%2.%3)</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/FWStatusDlg.cpp" line="483"/>
+        <source>Can&apos;t connect to client...</source>
+        <translation>Kan inte ansluta till klienten…</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/FWStatusDlg.cpp" line="569"/>
+        <source>The verify command has been sent to the client</source>
+        <translatorcomment>Crappy translation</translatorcomment>
+        <translation>Klienten har blivit ombedd att verifiera</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/FWStatusDlg.cpp" line="571"/>
+        <source>Failed to send the verify message to this client</source>
+        <translatorcomment>Crappy translation</translatorcomment>
+        <translation>Lyckades inte be klienten att verifiera</translation>
+    </message>
+</context>
+<context>
+    <name>CSimplePasswordDlg</name>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/SimplePasswordDlg.cpp" line="18"/>
+        <source>Password:</source>
+        <translation>Lösenord:</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/SimplePasswordDlg.cpp" line="19"/>
+        <source>Please enter the password needed to change the 
+settings for this client</source>
+        <translation>Ange lösenord för att ändra klientens inställningar</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/SimplePasswordDlg.cpp" line="55"/>
+        <source>Password Incorrect!</source>
+        <translation>Felaktigt lösenord!</translation>
+    </message>
+</context>
+<context>
+    <name>FWProgressDialog</name>
+    <message>
+        <location filename="../progressdialog/progressdialog.cpp" line="156"/>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+</context>
+<context>
+    <name>KioskCategoryTreeModel</name>
+    <message>
+        <location filename="../KioskCategoryTreeModel.cpp" line="54"/>
+        <source>Category</source>
+        <translation>Kategori</translation>
+    </message>
+    <message>
+        <location filename="../KioskCategoryTreeModel.cpp" line="54"/>
+        <source>Description</source>
+        <translation>Beskrivning</translation>
+    </message>
+    <message>
+        <location filename="../KioskCategoryTreeModel.cpp" line="64"/>
+        <source>All</source>
+        <translation>Alla</translation>
+    </message>
+</context>
+<context>
+    <name>LogViewer</name>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/LogViewer.cpp" line="22"/>
+        <location filename="../ClientStatusMonitorFiles/LogViewer.cpp" line="40"/>
+        <source>Client Log</source>
+        <translation>Klient-log</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/LogViewer.cpp" line="111"/>
+        <source>Sorry, but getting the log file failed.</source>
+        <translation>Tyvärr, det gick inte hämta logfilen.</translation>
+    </message>
+</context>
+<context>
+    <name>Options</name>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Options.ui" line="14"/>
+        <source>Options</source>
+        <translation>Alternativ</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Options.ui" line="23"/>
+        <source>Debug Level:</source>
+        <translation>Debug-nivå:</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Options.ui" line="43"/>
+        <source>File Check Interval:</source>
+        <translation>Intervall för filkontroll:</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Options.ui" line="65"/>
+        <source>minutes</source>
+        <translation>minuter</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Options.ui" line="74"/>
+        <source>Free Space Margin:</source>
+        <translation>Gräns för ledigt utrymme:</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Options.ui" line="81"/>
+        <source> MB</source>
+        <translation> MB</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Options.ui" line="97"/>
+        <source>Password:</source>
+        <translation>Lösenord:</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Options.ui" line="111"/>
+        <source>Confirm Password:</source>
+        <translation>Bekräfta lösenord:</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Options.ui" line="125"/>
+        <source>Priority:</source>
+        <translation>Prioritet:</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Options.cpp" line="37"/>
+        <source>Win</source>
+        <translatorcomment>???</translatorcomment>
+        <translation>Win</translation>
+    </message>
+</context>
+<context>
+    <name>PreferencesMain</name>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/PreferencesMain.cpp" line="54"/>
+        <source>Communications</source>
+        <translation>Kommunikation</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/PreferencesMain.cpp" line="56"/>
+        <source>Boosters</source>
+        <translatorcomment>How to translate?</translatorcomment>
+        <translation>Boosters</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/PreferencesMain.cpp" line="58"/>
+        <source>Options</source>
+        <translation>Alternativ</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/PreferencesMain.cpp" line="67"/>
+        <source>FileWave™ Client Preferences</source>
+        <translation>FileWave™ klientinställningar</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/PreferencesMain.cpp" line="259"/>
+        <source>Cannot confirm password.  Please re-enter password.</source>
+        <translation>Kan inte bekräfta lösenordet.  Försök igen.</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/PreferencesMain.cpp" line="260"/>
+        <source>Error</source>
+        <translation>Fel</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../KioskResourceHelper.cpp" line="48"/>
+        <source>Select an Icon</source>
+        <translation>Välj en ikon</translation>
+    </message>
+    <message>
+        <location filename="../KioskResourceHelper.cpp" line="52"/>
+        <source>Select an exe, dll, or icon file</source>
+        <translation>Välj en exe, dll, eller ikon-fil</translation>
+    </message>
+    <message>
+        <location filename="../KioskResourceHelper.cpp" line="56"/>
+        <source>Select an Application or icon file</source>
+        <translation>Välj ett program, eller en ikon-fil</translation>
+    </message>
+    <message>
+        <location filename="../KioskResourceHelper.cpp" line="59"/>
+        <source>Images (</source>
+        <translation>Bilder (</translation>
+    </message>
+    <message>
+        <location filename="../ClientStatusMonitorFiles/Communications.cpp" line="55"/>
+        <source>(no value)</source>
+        <translation>(inget värde)</translation>
+    </message>
+    <message>
+        <location filename="../kdbytesize.cpp" line="52"/>
+        <source>B</source>
+        <translation>B</translation>
+    </message>
+    <message>
+        <location filename="../kdbytesize.cpp" line="53"/>
+        <source>kB</source>
+        <translation>kB</translation>
+    </message>
+    <message>
+        <location filename="../kdbytesize.cpp" line="54"/>
+        <source>MB</source>
+        <translation>MB</translation>
+    </message>
+    <message>
+        <location filename="../kdbytesize.cpp" line="55"/>
+        <source>GB</source>
+        <translation>GB</translation>
+    </message>
+    <message>
+        <location filename="../kdbytesize.cpp" line="56"/>
+        <source>TB</source>
+        <translation>TB</translation>
+    </message>
+</context>
+<context>
+    <name>SearchLineEdit</name>
+    <message>
+        <location filename="../searchlineedit/searchlineedit.cpp" line="19"/>
+        <source>Search...</source>
+        <translation>Sök…</translation>
+    </message>
+</context>
+</TS>
