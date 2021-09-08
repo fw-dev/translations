336f76b5397076204b2898677ffd11a0b7dd809e
Pierre-Nicolas Rigal <pierrenicolasr@filewave.com>
2021-09-08 15:47:09 +0200

    FW-0000 update with translator files

--- ts_files/super_prefs_zh_CN.ts
+++ ts_files/super_prefs_zh_CN.ts
@@ -6,37 +6,37 @@
     <message>
         <location filename="../../QtSuperPrefsEditor/main.cpp" line="21"/>
         <source>FileWave</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">FileWave</translation>
     </message>
     <message>
         <location filename="../../QtSuperPrefsEditor/main.cpp" line="22"/>
         <source>filewave.com</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">filewave.com</translation>
     </message>
     <message>
         <location filename="../../QtSuperPrefsEditor/main.cpp" line="23"/>
         <source>SuperPrefs Editor</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">SuperPrefs 编辑器</translation>
     </message>
     <message>
         <location filename="../../QtSuperPrefsEditor/main.cpp" line="32"/>
         <source>File</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">文件</translation>
     </message>
     <message>
         <location filename="../../QtSuperPrefsEditor/main.cpp" line="33"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">关闭</translation>
     </message>
     <message>
         <location filename="../../QtSuperPrefsEditor/main.cpp" line="34"/>
         <source>Ctrl+W</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ctrl+W</translation>
     </message>
     <message>
         <location filename="../../QtSuperPrefsEditor/main.cpp" line="35"/>
         <source>About %1</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">关于 %1</translation>
     </message>
 </context>
 </TS>
