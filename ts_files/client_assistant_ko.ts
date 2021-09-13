d40187ae509c9ec57b33a827638ac0c4ebde385c
Pierre-Nicolas Rigal <pierrenicolasr@filewave.com>
2021-09-13 10:53:04 +0200

    FW-0000 add KO ts files

--- ts_files/client_assistant_ko.ts
+++ ts_files/client_assistant_ko.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="../../QtClientAssistant/AssistantCustomDataDlg.ui" line="14"/>
         <source>Custom Data Fields</source>
-        <translation type="unfinished"></translation>
+        <translation>사용자 지정 데이터 필드</translation>
     </message>
     <message>
         <location filename="../../QtClientAssistant/AssistantCustomDataDlg.ui" line="28"/>
         <source>Department</source>
-        <translation type="unfinished"></translation>
+        <translation>부서</translation>
     </message>
     <message>
         <location filename="../../QtClientAssistant/AssistantCustomDataDlg.ui" line="35"/>
         <source>Location</source>
-        <translation type="unfinished"></translation>
+        <translation>위치</translation>
     </message>
     <message>
         <location filename="../../QtClientAssistant/AssistantCustomDataDlg.ui" line="42"/>
         <source>Building</source>
-        <translation type="unfinished"></translation>
+        <translation>빌딩</translation>
     </message>
     <message>
         <location filename="../../QtClientAssistant/AssistantCustomDataDlg.ui" line="49"/>
         <source>Monitor ID</source>
-        <translation type="unfinished"></translation>
+        <translation>모니터 ID</translation>
     </message>
 </context>
 <context>
@@ -34,62 +34,62 @@
     <message>
         <location filename="../../QtClientAssistant/assistantdlg.ui" line="14"/>
         <source>FileWave Client Installer Assistant</source>
-        <translation type="unfinished"></translation>
+        <translation>FileWave 클라이언트 설치 관리자</translation>
     </message>
     <message>
         <location filename="../../QtClientAssistant/assistantdlg.ui" line="37"/>
         <source>Welcome to the FileWave Client Setup Assistant</source>
-        <translation type="unfinished"></translation>
+        <translation>FileWave 클라이언트 설치 관리자에 오신 것을 환영합니다</translation>
     </message>
     <message>
         <location filename="../../QtClientAssistant/assistantdlg.ui" line="47"/>
         <source>Please enter the initial preferences for this Client</source>
-        <translation type="unfinished"></translation>
+        <translation>이 클라이언트의 초기 환경설정을 입력하십시오.</translation>
     </message>
     <message>
         <location filename="../../QtClientAssistant/assistantdlg.ui" line="70"/>
         <source>Address:</source>
-        <translation type="unfinished"></translation>
+        <translation>주소:</translation>
     </message>
     <message>
         <location filename="../../QtClientAssistant/assistantdlg.ui" line="77"/>
         <source>Port:</source>
-        <translation type="unfinished"></translation>
+        <translation>포트:</translation>
     </message>
     <message>
         <location filename="../../QtClientAssistant/assistantdlg.ui" line="90"/>
         <source>Server:</source>
-        <translation type="unfinished"></translation>
+        <translation>서버:</translation>
     </message>
     <message>
         <location filename="../../QtClientAssistant/assistantdlg.ui" line="123"/>
         <source>Booster:</source>
-        <translation type="unfinished"></translation>
+        <translation>부스터:</translation>
     </message>
     <message>
         <location filename="../../QtClientAssistant/assistantdlg.ui" line="200"/>
         <source>Use Computer Name for Client Name</source>
-        <translation type="unfinished"></translation>
+        <translation>클라이언트 이름에 컴퓨터 이름 사용</translation>
     </message>
     <message>
         <location filename="../../QtClientAssistant/assistantdlg.ui" line="248"/>
         <source>Client Name:</source>
-        <translation type="unfinished"></translation>
+        <translation>클라이언트 이름:</translation>
     </message>
     <message>
         <location filename="../../QtClientAssistant/assistantdlg.ui" line="271"/>
         <source>Client Password:</source>
-        <translation type="unfinished"></translation>
+        <translation>클라이언트 비빌번호:</translation>
     </message>
     <message>
         <location filename="../../QtClientAssistant/assistantdlg.ui" line="297"/>
         <source>Confirm Password:</source>
-        <translation type="unfinished"></translation>
+        <translation>비밀번호 확인:</translation>
     </message>
     <message>
         <location filename="../../QtClientAssistant/assistantdlg.ui" line="343"/>
         <source>Edit Custom Data...</source>
-        <translation type="unfinished"></translation>
+        <translation>사용자 지정 데이터 편집...</translation>
     </message>
 </context>
 <context>
@@ -97,22 +97,22 @@
     <message>
         <location filename="../../QtClientAssistant/assistantdlg.cpp" line="221"/>
         <source>The password and confirmation don&apos;t match -- please try again.</source>
-        <translation type="unfinished"></translation>
+        <translation>비밀번호와 확인 비밀번호가 일치하지 않습니다. 다시 시도하십시오.</translation>
     </message>
     <message>
         <location filename="../../QtClientAssistant/main.cpp" line="19"/>
         <source>FileWave</source>
-        <translation type="unfinished"></translation>
+        <translation>FileWave</translation>
     </message>
     <message>
         <location filename="../../QtClientAssistant/main.cpp" line="20"/>
         <source>filewave.com</source>
-        <translation type="unfinished"></translation>
+        <translation>filewave.com</translation>
     </message>
     <message>
         <location filename="../../QtClientAssistant/main.cpp" line="21"/>
         <source>FileWave Client Assistant</source>
-        <translation type="unfinished"></translation>
+        <translation>FileWave 클라이언트 관리자</translation>
     </message>
 </context>
 </TS>
--- ts_files/client_ko.ts
+++ ts_files/client_ko.ts
@@ -6,119 +6,119 @@
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="489"/>
         <source>Checking for new model version</source>
-        <translation type="unfinished"></translation>
+        <translation>새 모델 버전을 확인하는 중</translation>
     </message>
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="511"/>
         <source>Check for new model in %1 seconds</source>
-        <translation type="unfinished"></translation>
+        <translation>%1초 후에 새 모델이 있는지 확인</translation>
     </message>
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="793"/>
         <source>Checking Files...</source>
-        <translation type="unfinished"></translation>
+        <translation>파일 확인 중...</translation>
     </message>
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="794"/>
         <source>Verifying Files...</source>
-        <translation type="unfinished"></translation>
+        <translation>파일 검증 중...</translation>
     </message>
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="813"/>
         <source>Verifying %1 of %2 filesets</source>
-        <translation type="unfinished"></translation>
+        <translation>%2개 파일 세트 중 %1 확인하는 중</translation>
     </message>
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="877"/>
         <source>Running..</source>
-        <translation type="unfinished"></translation>
+        <translation>실행 중...</translation>
     </message>
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="879"/>
         <source>Finished Verifying Files...</source>
-        <translation type="unfinished"></translation>
+        <translation>파일 검증 완료...</translation>
     </message>
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="1489"/>
         <source>Downloading User Manifest</source>
-        <translation type="unfinished"></translation>
+        <translation>사용자 매니페스트 다운로드 중</translation>
     </message>
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="1517"/>
         <source>Downloading Imaging Manifest</source>
-        <translation type="unfinished"></translation>
+        <translation>이미징 매니페스트 다운로드 중</translation>
     </message>
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="1544"/>
         <source>Downloading Smart Filter Manifests</source>
-        <translation type="unfinished"></translation>
+        <translation>스마트 필터 매니페스트 다운로드 중</translation>
     </message>
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="1611"/>
         <location filename="../../QtClient/Catalog.cpp" line="1629"/>
         <location filename="../../QtClient/Catalog.cpp" line="1987"/>
         <source>Running</source>
-        <translation type="unfinished"></translation>
+        <translation>실행 중</translation>
     </message>
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="3881"/>
         <source>Waiting for Booster - %1</source>
-        <translation type="unfinished"></translation>
+        <translation>부스터 - %1 대기 중</translation>
     </message>
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="3900"/>
         <source>%1 (or dependency of) not found on server</source>
-        <translation type="unfinished"></translation>
+        <translation>서버에서 %1(또는 종속 항목)을(를) 찾을 수 없음</translation>
     </message>
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="4250"/>
         <source>Downloading Fileset Container (%1)</source>
-        <translation type="unfinished"></translation>
+        <translation>파일 세트 컨테이너(%1) 다운로드 중</translation>
     </message>
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="4252"/>
         <source>Processing requirements of %1</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 요구 사항 처리 중</translation>
     </message>
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="4361"/>
         <source>The Hard Disk is Full...Cannot download new Filesets.</source>
-        <translation type="unfinished"></translation>
+        <translation>하드 디스크가 꽉 찼습니다...새 파일 세트를 다운로드할 수 없습니다.</translation>
     </message>
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="4373"/>
         <source>Downloading fileset %1 of %2</source>
-        <translation type="unfinished"></translation>
+        <translation>파일 세트 %2 중 %1 다운로드 중</translation>
     </message>
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="4412"/>
         <source>Activating %1</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 활성화 중</translation>
     </message>
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="4422"/>
         <source>Activating fileset %1 of %2</source>
-        <translation type="unfinished"></translation>
+        <translation>파일 세트 %2 중 %1 활성화하는 중</translation>
     </message>
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="4525"/>
         <source>Installing Software...</source>
-        <translation type="unfinished"></translation>
+        <translation>소프트웨어 설치 중...</translation>
     </message>
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="4617"/>
         <source> Installation Failed</source>
-        <translation type="unfinished"></translation>
+        <translation> 설치 실패</translation>
     </message>
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="4625"/>
         <source> Installation Successfully</source>
-        <translation type="unfinished"></translation>
+        <translation> 설치 성공</translation>
     </message>
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="4976"/>
         <source>Updating %1 to new version</source>
-        <translation type="unfinished"></translation>
+        <translation>%1을(를) 새 버전으로 업데이트하는 중</translation>
     </message>
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="5540"/>
@@ -128,7 +128,12 @@
 **************************
 User ID: %1
 </source>
-        <translation type="unfinished"></translation>
+        <translation>
+**************************
+**FileWave 클라이언트 상태**
+**************************
+사용자 ID: %1
+</translation>
     </message>
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="5545"/>
@@ -137,14 +142,20 @@ User ID: %1
 Filesets in Inventory:
 
 </source>
-        <translation type="unfinished"></translation>
+        <translation>현재 모델 번호: %1
+
+인벤토리의 파일 세트:
+
+</translation>
     </message>
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="5561"/>
         <source>Filesets not meeting requirements:
 
 </source>
-        <translation type="unfinished"></translation>
+        <translation>파일 세트가 요구 사항을 충족하지 않음:
+
+</translation>
     </message>
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="5576"/>
@@ -152,111 +163,114 @@ Filesets in Inventory:
 Worklist:
 
 </source>
-        <translation type="unfinished"></translation>
+        <translation>
+작업 목록:
+
+</translation>
     </message>
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="5586"/>
         <source>&lt;Fileset name still unknown&gt; ID:%1, revision ID:%2</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;파일 세트 이름을 아직 알 수 없음 &gt; ID: %1, 수정 ID: %2</translation>
     </message>
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="5656"/>
         <source>This computer will be shut down once its data is erased</source>
-        <translation type="unfinished"></translation>
+        <translation>데이터가 삭제되면 이 컴퓨터는 종료됩니다</translation>
     </message>
     <message>
         <location filename="../../QtClient/Catalog.cpp" line="5657"/>
         <source>Executing Remote Wipe</source>
-        <translation type="unfinished"></translation>
+        <translation>원격 삭제 실행 중</translation>
     </message>
 </context>
 <context>
     <name>FW::FilesetContainer</name>
     <message>
-        <location filename="../../QtClient/FilesetContainer.cpp" line="453"/>
+        <location filename="../../QtClient/FilesetContainer.cpp" line="450"/>
         <source>Fileset </source>
-        <translation type="unfinished"></translation>
+        <translation>파일 세트 </translation>
     </message>
     <message>
-        <location filename="../../QtClient/FilesetContainer.cpp" line="465"/>
+        <location filename="../../QtClient/FilesetContainer.cpp" line="462"/>
         <source>revision </source>
-        <translation type="unfinished"></translation>
+        <translation>수정 </translation>
     </message>
     <message>
-        <location filename="../../QtClient/FilesetContainer.cpp" line="498"/>
+        <location filename="../../QtClient/FilesetContainer.cpp" line="495"/>
         <source>Processing %1</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 처리 중</translation>
     </message>
     <message>
-        <location filename="../../QtClient/FilesetContainer.cpp" line="1435"/>
+        <location filename="../../QtClient/FilesetContainer.cpp" line="1432"/>
         <source>Deleting empty folders of %1</source>
-        <translation type="unfinished"></translation>
+        <translation>%1의 빈 폴더 삭제 중</translation>
     </message>
     <message>
-        <location filename="../../QtClient/FilesetContainer.cpp" line="1481"/>
-        <location filename="../../QtClient/FilesetContainer.cpp" line="1602"/>
+        <location filename="../../QtClient/FilesetContainer.cpp" line="1478"/>
+        <location filename="../../QtClient/FilesetContainer.cpp" line="1599"/>
         <source>Making %1 passive</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 패시브로 만들기</translation>
     </message>
     <message>
-        <location filename="../../QtClient/FilesetContainer.cpp" line="1673"/>
+        <location filename="../../QtClient/FilesetContainer.cpp" line="1670"/>
         <source>Deleting %1</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 삭제 중</translation>
     </message>
     <message>
-        <location filename="../../QtClient/FilesetContainer.cpp" line="1735"/>
-        <location filename="../../QtClient/FilesetContainer.cpp" line="1797"/>
+        <location filename="../../QtClient/FilesetContainer.cpp" line="1732"/>
+        <location filename="../../QtClient/FilesetContainer.cpp" line="1794"/>
         <source>Executing %1...</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 실행 중...</translation>
     </message>
     <message>
-        <location filename="../../QtClient/FilesetContainer.cpp" line="1737"/>
+        <location filename="../../QtClient/FilesetContainer.cpp" line="1734"/>
         <source>Executing Files...</source>
-        <translation type="unfinished"></translation>
+        <translation>파일 실행 중...</translation>
     </message>
     <message>
-        <location filename="../../QtClient/FilesetContainer.cpp" line="1860"/>
+        <location filename="../../QtClient/FilesetContainer.cpp" line="1857"/>
         <source>Checking Files..</source>
-        <translation type="unfinished"></translation>
+        <translation>파일 확인 중..</translation>
     </message>
     <message>
-        <location filename="../../QtClient/FilesetContainer.cpp" line="1901"/>
+        <location filename="../../QtClient/FilesetContainer.cpp" line="1898"/>
         <source>Activating Files..</source>
-        <translation type="unfinished"></translation>
+        <translation>파일 활성화 중..</translation>
     </message>
     <message>
-        <location filename="../../QtClient/FilesetContainer.cpp" line="1982"/>
+        <location filename="../../QtClient/FilesetContainer.cpp" line="1979"/>
         <source>Running..</source>
-        <translation type="unfinished"></translation>
+        <translation>실행 중...</translation>
     </message>
 </context>
 <context>
     <name>FW::MainController</name>
     <message>
-        <location filename="../../QtClient/MainController.cpp" line="224"/>
+        <location filename="../../QtClient/MainController.cpp" line="223"/>
         <source>Client is archived.</source>
-        <translation type="unfinished"></translation>
+        <translation>클라이언트가 보관되었습니다.</translation>
     </message>
     <message>
-        <location filename="../../QtClient/MainController.cpp" line="229"/>
+        <location filename="../../QtClient/MainController.cpp" line="228"/>
         <source>Running...</source>
-        <translation type="unfinished"></translation>
+        <translation>실행 중...</translation>
     </message>
     <message>
-        <location filename="../../QtClient/MainController.cpp" line="234"/>
+        <location filename="../../QtClient/MainController.cpp" line="233"/>
         <source>Installing %1</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 설치 중</translation>
     </message>
     <message>
-        <location filename="../../QtClient/MainController.cpp" line="239"/>
+        <location filename="../../QtClient/MainController.cpp" line="238"/>
         <location filename="../../QtClient/MainController.cpp" line="332"/>
         <source>Blocked by local restriction</source>
-        <translation type="unfinished"></translation>
+        <translation>로컬 제한에 의해 차단됨</translation>
     </message>
     <message>
-        <location filename="../../QtClient/MainController.cpp" line="355"/>
+        <location filename="../../QtClient/MainController.cpp" line="348"/>
         <source>Connecting to FW Server...</source>
-        <translation type="unfinished"></translation>
+        <translation>FW 서버에 연결 중...</translation>
     </message>
 </context>
 <context>
@@ -264,28 +278,28 @@ Worklist:
     <message>
         <location filename="../../QtClient/ServerConnection.cpp" line="1375"/>
         <source>Building list of files to download...</source>
-        <translation type="unfinished"></translation>
+        <translation>다운로드할 파일 목록 작성 중...</translation>
     </message>
     <message>
         <location filename="../../QtClient/ServerConnection.cpp" line="1394"/>
         <source>Downloading subset for %1</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 하위 집합 다운로드 중</translation>
     </message>
     <message>
         <location filename="../../QtClient/ServerConnection.cpp" line="1452"/>
         <location filename="../../QtClient/ServerConnection.cpp" line="1685"/>
         <source>Downloading %1</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 다운로드 중</translation>
     </message>
     <message>
         <location filename="../../QtClient/ServerConnection.cpp" line="1602"/>
         <source>Sending download request to server...</source>
-        <translation type="unfinished"></translation>
+        <translation>서버로 다운로드 요청을 보내는 중...</translation>
     </message>
     <message>
         <location filename="../../QtClient/ServerConnection.cpp" line="1686"/>
         <source>Downloading...</source>
-        <translation type="unfinished"></translation>
+        <translation>다운로드 중...</translation>
     </message>
 </context>
 <context>
@@ -293,7 +307,7 @@ Worklist:
     <message>
         <location filename="../../QtClient/kiosk/KioskServerController.cpp" line="361"/>
         <source>Installing Software...</source>
-        <translation type="unfinished"></translation>
+        <translation>소프트웨어 설치 중...</translation>
     </message>
 </context>
 <context>
@@ -301,54 +315,54 @@ Worklist:
     <message>
         <location filename="../../QtClient/ActionContainer.cpp" line="96"/>
         <source>Fileset </source>
-        <translation type="unfinished"></translation>
+        <translation>파일 세트 </translation>
     </message>
     <message>
         <location filename="../../QtClient/ActionContainer.cpp" line="106"/>
         <source>revision </source>
-        <translation type="unfinished"></translation>
+        <translation>수정 </translation>
     </message>
     <message>
         <location filename="../../QtClient/ActionContainer.cpp" line="126"/>
         <source>checkRequirements</source>
         <comment>ActionRecord</comment>
-        <translation type="unfinished"></translation>
+        <translation>checkRequirements</translation>
     </message>
     <message>
         <location filename="../../QtClient/ActionContainer.cpp" line="127"/>
         <source>downloadFiles</source>
         <comment>ActionRecord</comment>
-        <translation type="unfinished"></translation>
+        <translation>downloadFiles</translation>
     </message>
     <message>
         <location filename="../../QtClient/ActionContainer.cpp" line="128"/>
         <source>activateFiles</source>
         <comment>ActionRecord</comment>
-        <translation type="unfinished"></translation>
+        <translation>activateFiles</translation>
     </message>
     <message>
         <location filename="../../QtClient/ActionContainer.cpp" line="129"/>
         <source>makeFilesPassive</source>
         <comment>ActionRecord</comment>
-        <translation type="unfinished"></translation>
+        <translation>makeFilesPassive</translation>
     </message>
     <message>
         <location filename="../../QtClient/ActionContainer.cpp" line="130"/>
         <source>updateFileset</source>
         <comment>ActionRecord</comment>
-        <translation type="unfinished"></translation>
+        <translation>updateFileset</translation>
     </message>
     <message>
         <location filename="../../QtClient/ActionContainer.cpp" line="131"/>
         <source>deleteFiles</source>
         <comment>ActionRecord</comment>
-        <translation type="unfinished"></translation>
+        <translation>deleteFiles</translation>
     </message>
     <message>
         <location filename="../../QtClient/ActionContainer.cpp" line="132"/>
         <source>updateFilesetDependencies</source>
         <comment>ActionRecord</comment>
-        <translation type="unfinished"></translation>
+        <translation>updateFilesetDependencies</translation>
     </message>
 </context>
 </TS>
